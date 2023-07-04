89ea4ae58e46acdc14fe00b509550f5b18eba56d
michelaproietti <mproietti@diag.uniroma1.it>
2023-07-04 18:36:49 +0200

    adding files in right directory

--- src/main.py
+++ src/main.py
@@ -1,5 +1,15 @@
-from preprocess_text import *
+from omegaconf import OmegaConf
+
+from preprocess import *
 from settings import *
+from data import *
+
+args = OmegaConf.load(ROOT_DIR+'/configs/config.yaml')
 
 if __name__ == '__main__':
-    preprocess_jsonl( f'{REPORTS_DIR}/Train.jsonl', f'{REPORT_PREPROCESS_DIR}/train_output.jsonl')
+    if args.opts.preprocess:
+        preprocess_jsonl( f'{REPORTS_DIR}/Train.jsonl', f'{REPORT_PREPROCESS_DIR}/train_output.jsonl', IMG_DIR_TRAIN)
+        preprocess_jsonl( f'{REPORTS_DIR}/Train.jsonl', f'{REPORT_PREPROCESS_DIR}/train_output.jsonl', IMG_DIR_VALID)
+        preprocess_jsonl( f'{REPORTS_DIR}/Train.jsonl', f'{REPORT_PREPROCESS_DIR}/train_output.jsonl', IMG_DIR_TEST)
+        
+    dataset = MIMIC_CXR(REPORT_PREPROCESS_DIR+'/train_output.jsonl', IMG_DIR_TRAIN)
--- src/settings.py
+++ src/settings.py
@@ -10,13 +10,13 @@ DATA_DIR = os.path.join(ROOT_DIR + '/data')
 MIMIC_DIR = os.path.join(DATA_DIR + '/mimic')
 
 IMG_DIR = os.path.join(MIMIC_DIR + '/images')
-IMG_DIR_TRAIN = os.path.join(MIMIC_DIR + '/Train')
-IMG_DIR_TEST = os.path.join(MIMIC_DIR + '/Test')
-IMG_DIR_VALID = os.path.join(MIMIC_DIR + '/Valid')
+IMG_DIR_TRAIN = os.path.join(IMG_DIR + '/Train')
+IMG_DIR_TEST = os.path.join(IMG_DIR + '/Test')
+IMG_DIR_VALID = os.path.join(IMG_DIR + '/Valid')
 REPORTS_DIR = os.path.join(MIMIC_DIR + '/reports')
 REPORT_PREPROCESS_DIR = os.path.join(REPORTS_DIR + '/preprocessed')
 
-list_dir = [DATA_DIR]
+list_dir = [DATA_DIR,REPORT_PREPROCESS_DIR]
 
 for x in list_dir:
     if not os.path.exists(x):
