924cbf0d497ed7dd438d3d51fb631af238152733
michelaproietti <mproietti@diag.uniroma1.it>
2023-12-12 16:05:33 +0100

    changing patch computation method

--- src/model_visual.py
+++ src/model_visual.py
@@ -4,6 +4,7 @@ from torch import nn
 import torch.nn.functional as F
 from collections import OrderedDict
 import numpy as np
+import matplotlib.pyplot as plt
 
 class ResNet50(nn.Module):
     def __init__(self, hid_dim, out_dim, dropout):
@@ -32,7 +33,21 @@ class ResNet50(nn.Module):
         out = self.out_fc(features)
         
         return features, out
-    
+
+def find_most_salient_patch(saliency_map, patch_size=64, stride=1):
+
+    pooled_saliency = F.avg_pool2d(saliency_map.unsqueeze(0), kernel_size=patch_size, stride=stride)
+    max_value, indices = torch.max(pooled_saliency.view(-1), dim=0)
+    index_1d = indices.item()
+
+    height, width = saliency_map.shape
+    ycord = index_1d // width
+    xcord = index_1d % width
+
+    top_left_x = xcord * stride
+    top_left_y = ycord * stride
+
+    return top_left_x, top_left_y
     
 def extract_patch_features(image_model, images, labels, patch_size, feat_size):
     # Get batch predictions
@@ -56,17 +71,14 @@ def extract_patch_features(image_model, images, labels, patch_size, feat_size):
                 # Get the model's predictions
                 _, logits = image_model(image.unsqueeze(0).to(device))
                 
-                # Get 
+                # Get saliency
                 cam = cam_extractor(j, logits)[0]
                 resize = torchvision.transforms.Resize((image.shape[1], image.shape[2]), interpolation=PIL.Image.BICUBIC)
                 mask = resize(cam).squeeze(0)
-
-                #result = overlay_mask(to_pil_image(image), to_pil_image(cam.squeeze(0), mode='F'), alpha=0.5)
-        
-                y, x = np.unravel_index(np.argmax(mask), mask.shape)
     
                 # Extract most activated patch_size x patch_size patch in the original image
                 patch = image.numpy().transpose(1, 2, 0)
+                topx, topy = find_most_salient_patch(mask)
                 patch = patch[y:y+patch_size, x:x+patch_size]
                 patch = patch / np.max(patch)
                 
@@ -75,6 +87,6 @@ def extract_patch_features(image_model, images, labels, patch_size, feat_size):
                 
                 # Sum features extracted from single patches
                 features = features + features_j
-                
+
         batch_features[i] = features
-    return torch.cat((batch_features, features_image), dim=1) # shape [batch_size, 2048]
+        return torch.cat((batch_features, features_image), dim=1) # shape [batch_size, 2048]
